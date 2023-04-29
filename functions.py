4d409cc66e35af45dff38f49295165d7d8e8c232
O-App-Pesi <sullivanskandi@outlook.com>
2023-04-29 19:35:16 +1000

    added entrance function

--- functions.py
+++ functions.py
@@ -6,3 +6,21 @@ def write_block():
 
 def prayer_box():
     print("pray to the god of this shrine")
+
+def entrance():
+    print("Welcome to KaiShin Shrine!")
+    print("A large Tori gate stands before you,")
+    print("as you walk through it you notice a sign ahead.")
+    print("The sign reads: 'North you can find fortunes'")
+    print("'East you can find wishing blocks'")
+    print("'West you can pray to the gods of this shrine'")
+    direction = ""
+    while direction != "North" or "East" or "West":
+        direction = input("Where would you like to go?")
+        if direction == "North":
+            pull_fortune()
+        elif direction == "East":
+            write_block()
+        elif direction == "West":
+            prayer_box()
+    
