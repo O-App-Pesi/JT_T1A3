0587fa1ddf55f2d9cb3d3b081fc3daee8cbf57bf
O-App-Pesi <sullivanskandi@outlook.com>
2023-04-30 11:17:37 +1000

    added colored package

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import random
+
 def pull_fortune():
     print("pull a fortune")
 
@@ -11,16 +13,18 @@ def entrance():
     print("Welcome to KaiShin Shrine!")
     print("A large Tori gate stands before you,")
     print("as you walk through it you notice a sign ahead.")
-    print("The sign reads: 'North you can find fortunes'")
-    print("'East you can find wishing blocks'")
-    print("'West you can pray to the gods of this shrine'")
+    print("'\u2191 North you can find fortunes'")
+    print("'\u21B1 East you can find wishing blocks'")
+    print("'\u21B0 West you can pray to the gods of this shrine'")
     direction = ""
-    while direction != "North" or "East" or "West":
-        direction = input("Where would you like to go?")
+    while direction == "":
+        direction = input("Where would you like to go?  ")
         if direction == "North":
             pull_fortune()
         elif direction == "East":
             write_block()
         elif direction == "West":
             prayer_box()
+
+
     
---
+++ main.py
@@ -0,0 +1,6 @@
+from colored import fg, bg, attr
+
+from functions import entrance
+
+entrance()
+
