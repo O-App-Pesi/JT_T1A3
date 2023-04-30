690436f9d9717e9b034b32eac5ee8dac0a131995
O-App-Pesi <sullivanskandi@outlook.com>
2023-04-30 17:11:22 +1000

    added if statements inside pull_fortune function for making decisions

--- functions.py
+++ functions.py
@@ -4,10 +4,30 @@ from colored import fg, bg, attr
 
 def pull_fortune():
     print("You arrive at a window where a shrine maiden is attending")
-    print("A box sits on the counter in front of the window")
+    print("A hexgonal cylinder sits on the counter in front of the window")
     print("If you shake, twist or roll this box, you can pull out a number")
     print("The shrine maiden will give you an omikuji (sacred fortune) in return...")
     user_continue = Confirm.ask("Would you like to pull a fortune?")
+    if user_continue:
+        print("You take the hexagonal cylinder in your hands...")
+        print("Here's your chance to find your destiny...")
+        print("Looks like you can shake, twist or roll it,")
+        print("There may be hidden actions as well...")
+        while user_continue:
+            action = Prompt.ask("What will you do?", 
+                                choices=["shake", "roll", "twist", "bash",
+                                         "juggle", "special"])
+            pull_number = 0
+            if action == "shake":
+                print("You shake the cylinder vigorously")
+                action_number = random.randint(6, 10)
+                pull_number += action_number
+                user_continue = Confirm.ask("Continue manipulating the cylinder?")
+            elif action == "roll":
+                print("You roll the cylinder on its side systematically")
+                action_number = random.randint(11, 15)
+                pull_number += action_number
+                user_continue = Confirm.ask("Continue manipulating the cylinder?")
 
 def write_block():
     print("write on wish block")
